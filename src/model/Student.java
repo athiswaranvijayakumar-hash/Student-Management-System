dc58862aab83e9c7979744570ee2433789a473d3
athiswaranvijayakumar-hash <athiswaranvijayakumar@gmail.com>
2026-07-10 21:19:34 +0530

    Add student model fields

--- src/model/Student.java
+++ src/model/Student.java
@@ -2,5 +2,10 @@ package model;
 
 
 public class Student {
+  private String studentId;
+  private String name;
+  private String department;
+  private String email;
+  private String phoneNumber;
   
 }
