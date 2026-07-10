3a1145e16da30ad6add941fba8cdb36ec73f2e58
athiswaranvijayakumar-hash <athiswaranvijayakumar@gmail.com>
2026-07-10 20:53:10 +0530

    update Main and Menu classes

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,11 @@
 package app;
-
-
+import menu.*;
 public class Main {
-  
+  public static void main(){
+    System.out.println("==============================");
+    System.out.println("  Student Management System.");
+    System.out.println("==============================");
+    Menu menu = new Menu();
+    menu.DisplayMenu();
+  }
 }
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,5 +1,12 @@
 package menu;
 
-public class Menu {
-  
+public class Menu{
+  public static void DisplayMenu(){
+  System.out.println("  1.Add Student Data.");
+  System.out.println("  2.Search Student Data.");
+  System.out.println("  3.Remove Student Data.");
+  System.out.println("  4.Update Student Data.");
+  System.out.println("  5.Exit.");
+  System.out.println("==============================");
+}
 }
