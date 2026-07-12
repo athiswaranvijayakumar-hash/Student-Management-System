c9a3dca76a4bec14e3f75bf70fa31461e5c6d6ba
athiswaranvijayakumar-hash <athiswaranvijayakumar@gmail.com>
2026-07-12 09:32:39 +0530

    Implement encapsulation in Student class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,29 @@
 package app;
 import menu.*;
+import java.util.*;
+import model.Student;
 public class Main {
-  public static void main(){
+  public static void main(String[] args) {
     System.out.println("==============================");
     System.out.println("  Student Management System.");
     System.out.println("==============================");
     Menu menu = new Menu();
     menu.DisplayMenu();
+    Scanner sc  = new Scanner(System.in);
+    int num = sc.nextInt();
+    Student student = new Student();
+    if(num==2) {
+    student.setStudentId("101");
+    student.setName("Athiswaran v");
+    student.setDepartment("AI&DS");
+    student.setEmail("athis@gmail.com");
+    student.setPhoneNumber("7845848503");
+    }
+    System.out.println(student.getStudentId());
+     System.out.println(student.getName());
+     System.out.println(student.getDepartment());
+     System.out.println(student.getEmail());
+     System.out.println(student.getPhoneNumber());
+     sc.close();
   }
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -7,5 +7,35 @@ public class Student {
   private String department;
   private String email;
   private String phoneNumber;
+  public void setStudentId(String studentId){
+    this.studentId = studentId;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+   public void setDepartment(String department) {
+    this.department = department;
+   }
+    public void setEmail(String email){
+    this.email = email;
+    }
+     public void setPhoneNumber(String phoneNumber){
+    this.phoneNumber = phoneNumber;
+     }
   
+  public String getStudentId(){
+     return studentId; 
+  }
+    public String getName(){
+     return name; 
+  }
+    public String getDepartment(){
+     return department; 
+  }
+    public String getEmail(){
+     return email; 
+  }
+    public String getPhoneNumber(){
+     return phoneNumber; 
+  }
 }
